8dba67a05b8a9973357d7f46666a3dfed6c6ab7c
Rayo0131 <motunrayo188@gmail.com>
2025-10-29 23:55:41 +0100

    second commit

--- myproject/models.py
+++ myproject/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from django.conf import settings
 
 
 # Create your models here.
 
 class Task(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='task')
     title = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
     description = models.TextField()
     completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
--- myproject/views.py
+++ myproject/views.py
@@ -1,13 +1,18 @@
 
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from .models import Task
 from .serializers import TaskSerializer
+from rest_framework import viewsets, permissions
 
 
 # Create your views here.
-@api_view(['GET'])
-def get_tasks(request):
-    tasks = Task.objects.all()
-    serializer = TaskSerializer(tasks, many=True)
-    return Response(serializer.data)
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def  get_queryset(self):
+        return Task.objects.filter(user=self.request.user).order_by('-created_at')
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+    
